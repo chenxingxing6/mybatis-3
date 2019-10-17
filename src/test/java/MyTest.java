3bcb55b368b12e0d884d2e71424cfc00ffcd7657
lanxinghua <lanxinghua@2dfire.com>
2019-10-17 17:55:47 +0800

    mybatis源码分析

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,10 @@
+/**
+ * User: lanxinghua
+ * Date: 2019/10/17 17:54
+ * Desc:
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("lanxinghua test");
+    }
+}
